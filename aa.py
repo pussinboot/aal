70d6ae4cfa2a45d0cbf4ed73f1b302489d24cea8
Leo <leooooo@bu.edu>
2015-03-27 21:14:23 -0400

    brain/backend complete
    getting there...
    input and learning of initial network working
    need to add testing (classifying) and figure out ux

--- aa.py
+++ aa.py
@@ -7,11 +7,54 @@ from urllib.request import urlretrieve
 import os
 os.environ['TKDND_LIBRARY'] = 'C:/Python34/Lib/tkdnd2.8/'
 from tkdnd_wrapper import TkDND
+from aal import *
 
-api_key = '874b1cf6420f724a52da51478cbf02f5' #public key no worries
-user = 'OJClock'
-n_pages = 2
+
 debug = False
+class AA:
+	def __init__(self):
+		self.username = 'OJClock' # mine
+		self.api_key = '874b1cf6420f724a52da51478cbf02f5' #public key no worries
+		self.n_pages = 2
+		self.library = []
+		self.ready_to_learn = False
+
+	def set_user(self,u):
+		self.user = u
+
+	def how_many(self,n):
+		self.n_pages = n // 50
+
+	def init_db(self):
+		print('initializing db')
+		for i in range(self.n_pages):
+			resp = requests.get("http://ws.audioscrobbler.com/2.0/?method=user.gettopalbums&user="+ self.username +"&page="+str(i+1)+ "&api_key="+self.api_key+"&format=json")
+			resp = resp.json()
+			#print(resp)
+			albums = resp['topalbums']['album']	
+			for a in albums:
+				self.library.append(Album(a['artist']['name'],a['name'],a['image']))
+		if debug:
+			for x in self.library:
+				print(x)
+				print(x.get_img('s'))
+				print(x.get_img('m'))
+				print(x.get_img('l'))
+				print(x.get_img('xl'))
+		self.ready_to_learn = True
+		print('done')
+
+	def learn(self):
+		if not self.ready_to_learn:
+			print('not ready to learn')
+			return
+		else:
+			self.brain = Brains(self.library)
+			self.brain.train()
+			print('woo')
+
+	def quit(self):
+		self.brain.quit()
 
 class Album:
 	def __init__(self,artist,album,images):
@@ -45,38 +88,24 @@ class Album:
 		return self.imagesource
 
 class Gui:
-	def __init__(self, master):        
+	def __init__(self, master, aa):        
+		self.aa = aa
+
+		def quitter():
+			self.aa.quit()
+			master.quit()
 
 		canvas = tkinter.Canvas(master,width=300,height=600)
 		canvas.grid(row = 0, column = 0)
 		canvas.grid_propagate(False)
-		library = []
-		for i in range(n_pages):
-			resp = requests.get("http://ws.audioscrobbler.com/2.0/?method=user.gettopalbums&user="+ user +"&page="+str(i+1)+ "&api_key="+api_key+"&format=json")
-			resp = resp.json()
-			#print(resp)
-			albums = resp['topalbums']['album']
-			
-			for a in albums:
-				#print(a)
-				library.append(Album(a['artist']['name'],a['name'],a['image']))
-		if debug:
-			for x in library:
-				print(x)
-				print(x.get_img('s'))
-				print(x.get_img('m'))
-				print(x.get_img('l'))
-				print(x.get_img('xl'))
-
-		print(library[0],library[0].get_src())
 		#canvas.create_image(0,0, anchor=tkinter.NW, image = tkinter.PhotoImage(file = library[1].get_img('xl')))
 		textbox = tkinter.Text(height=1)
 		dnd = TkDND(master)
 		textbox.place(x=0,y=327,width=300)
 
-		self.button1 = tkinter.Button(canvas,text='init database', width = 300, height = 75,command = lambda: print('a'),anchor=tkinter.NW)
-		self.button2 = tkinter.Button(canvas,text='test picture',width = 300, height = 75,command = lambda: print('b'),anchor=tkinter.NW)
-		self.button3 = tkinter.Button(canvas,text='exit',width = 300, height = 75,command = lambda: master.quit(),anchor=tkinter.NW)
+		self.button1 = tkinter.Button(canvas,text='init database', width = 300, height = 75,command = aa.init_db, anchor=tkinter.NW)
+		self.button2 = tkinter.Button(canvas,text='learn',width = 300, height = 75,command = aa.learn,anchor=tkinter.NW)
+		self.button3 = tkinter.Button(canvas,text='exit',width = 300, height = 75,command = quitter,anchor=tkinter.NW)
 		self.button1.place(x=0,y=375) 
 		self.button2.place(x=0,y=450)
 		self.button3.place(x=0,y=525)
@@ -94,10 +123,12 @@ class Gui:
 			test(content)
 			# do something
 
+
 		dnd.bindtarget(textbox, handle, 'text/uri-list')
 
 if __name__=='__main__':
+	aa = AA()
 	root = tkinter.Tk()
 	root.wm_resizable(0,0)
-	test = Gui(root)
+	test = Gui(root,aa)
 	root.mainloop()
